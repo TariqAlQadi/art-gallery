13e3e091d24311d88a235a2ad3195604fc4fd145
Yuri Padilla <yuripadilla017@gmail.com>
2023-01-09 14:53:32 +0100

    add spotlight

--- pages/spotlight.js
+++ pages/spotlight.js
@@ -1,7 +1,20 @@
 import useSWR from "swr";
+import ArtPiecePreview from "../components/ArtPiecePreview/ArtPiecePreview";
 
-const { data, error, isLoading } = useSWR(
-  "https://example-apis.vercel.app/api/art"
-);
+export default function Spotlight() {
+    function getRandomElement(array) {
+        return array[Math.floor(Math.random() * array.length)];
+    }
+    const { data, error, isLoading } = useSWR("https://example-apis.vercel.app/api/art");
 
-export default function Spotlight() {}
+    if (error) return <div>failed to load</div>;
+    if (isLoading) return <div>loading...</div>;
+
+    const randomPiece = getRandomElement(data);
+
+    console.log(randomPiece);
+
+    return (
+        <ArtPiecePreview image={randomPiece.imageSource} title={randomPiece.name} artist={randomPiece.artist} />
+    );
+}
