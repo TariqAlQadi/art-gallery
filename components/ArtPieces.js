5d12f39e1eddd2bcca346a1df12a262c7695c99e
TariqAlQadi <tariqalqadi@gmx.de>
2023-01-09 11:04:07 +0100

    first layout and list

---
+++ components/ArtPieces.js
@@ -0,0 +1,3 @@
+export default function ArtPieces({ pieces }) {
+  return <ul></ul>;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
+import useSWR from "swr";
+
 export default function HomePage() {
+  const { data, error, isLoading } = useSWR(
+    "https://example-apis.vercel.app/api/art"
+  );
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  console.log(data);
+
   return (
-    <div>
-      <h1>Hello from Next.js</h1>
-    </div>
+    <ul>
+      {data.map((artPiece) => {
+        return <li key={artPiece.slug}>{artPiece.artist}</li>;
+      })}
+    </ul>
   );
 }
