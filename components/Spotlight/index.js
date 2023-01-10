6978757242d62d880b36fcc06ff4eb575711602e
TariqAlQadi <tariqalqadi@gmx.de>
2023-01-10 21:29:53 +0100

    add link to spotlight and button fix

--- components/Spotlight/index.js
+++ components/Spotlight/index.js
@@ -1,16 +1,19 @@
 import Image from "next/image";
 import FavouriteButton from "../FavouriteButton";
+import Link from "next/link";
 
 export default function Spotlight({ piece }) {
   return (
     <section>
-      <p>{piece.artist}</p>
-      <Image
-        src={piece.imageSource}
-        width={piece.dimensions.width / 3}
-        height={piece.dimensions.height / 3}
-        alt={piece.name}
-      />
+      <Link href={`./art-pieces/${piece.slug}`}>
+        <p>{piece.artist}</p>
+        <Image
+          src={piece.imageSource}
+          width={piece.dimensions.width / 3}
+          height={piece.dimensions.height / 3}
+          alt={piece.name}
+        />
+      </Link>
       <FavouriteButton isFavorite onToggleFavorite />
     </section>
   );
--- pages/store.js
+++ pages/store.js
@@ -3,6 +3,6 @@ import { atom } from "jotai";
 const pieces = [];
 const globalPieces = atom(pieces);
 
-const artPiecesInfo = atom();
+const artPiecesInfo = atom([]);
 
 export default globalPieces;
